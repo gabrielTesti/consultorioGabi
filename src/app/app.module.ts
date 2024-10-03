7cff05c2c34973404043f836ee1f19c66ba5499a
gabrielTesti <gabrieltesti22@gmail.com>
2024-10-03 11:33:21 -0300

    login modal eliminado, login page creada en pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomePageComponent } from "./pages/welcome-page/welcome-page.component"
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { LoginModalComponent } from './pages/welcome-page/login-modal/login-modal.component';
+
 
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderWelcomeComponent } from './shared/header-welcome/header-welcome.component';
 import { HeaderMainComponent } from './shared/header-main/header-main.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
     RegisterPageComponent,
-    LoginModalComponent,
     FooterComponent,
     HeaderWelcomeComponent,
-    HeaderMainComponent
+    HeaderMainComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private loggedIn: boolean = true; // indica si el usuario esta logueado
+  private loggedIn: boolean = false; // indica si el usuario esta logueado
 
   constructor() { }
 
