0d45e123f1308bfded27e76abe94aecf1c7630fa
JuanPelu1999 <juanfarre99@gmail.com>
2024-10-05 12:11:35 -0300

    add main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 const routes: Routes = [
   {path: "welcome", component: WelcomePageComponent },
-  {path: '', redirectTo: 'welcome', pathMatch: 'full' }
+  {path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {path: "main", component: MainPageComponent}
   
  
   
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  
+
+  constructor(private authservice: AuthService) {}
+
+
+  ngOnInit(): void {
+    this.login(); 
+  }
+
+  login(): void {    
+    this.authservice.login();
+  }
 
 }
