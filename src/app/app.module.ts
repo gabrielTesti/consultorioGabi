b20ffca274b569ace3cabbbb4f6d5b81a091421f
gabrielTesti <gabrieltesti22@gmail.com>
2024-10-05 17:40:44 -0300

    cambios hechos en pacientes/nuevo-turno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -52,7 +53,8 @@ import { MisDatosComponent } from './pages/paciente/mis-datos/mis-datos.componen
     MatFormFieldModule,
     MatInputModule,
     MatDialogModule,
-    MatTooltipModule
+    MatTooltipModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/paciente/nuevo-turno/nuevo-turno.component.ts
+++ src/app/pages/paciente/nuevo-turno/nuevo-turno.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+interface Turno {
+  cobertura: string;
+  especialidad: string;
+  profesional: string;
+  fecha: string;
+  hora: string;
+  notas: string;
+}
 
 @Component({
   selector: 'app-nuevo-turno',
@@ -6,5 +18,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuevo-turno.component.css']
 })
 export class NuevoTurnoComponent {
+
+  showModal: boolean = false;
+
+  turnos: Turno[] = []; // almacena los turnos creados
+  constructor(private router: Router) {}
+
+  cobertura: string = '';
+  especialidad: string = '';
+  profesional: string = '';
+  fecha: string = '';
+  hora: string = '';
+  notas: string = '';
+
+  onSubmit(form: NgForm) {
+    if(form.valid)
+   this.showModal=true;
+    console.log('Turno creado:', {
+      cobertura: this.cobertura,
+      especialidad: this.especialidad,
+      profesional: this.profesional,
+      fecha: this.fecha,
+      hora: this.hora,
+      notas: this.notas
+    });  
+  }
+
+  cancelar() {
+    this.router.navigate(['/pacientes']);
+  }
+
+  closeModal() {
+    this.showModal = false; //cierra el modal
+  }
+  
+  confirm(){
+    this.router.navigate(['/pacientes/mis-turnos']);
+  }
+
+
+
+
 
 }
