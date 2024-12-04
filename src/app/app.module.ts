00334805d59b8d4955f89fff221b51b4e7c4b466
JuanFarre <juanfarre99@gmail.com>
2024-12-04 17:53:11 -0300

    modificamos el appmodule para que este en español las fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WelcomePageComponent } from "./pages/welcome-page/welcome-page.component"
+import { WelcomePageComponent } from "./pages/welcome-page/welcome-page.component";
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderWelcomeComponent } from './shared/header-welcome/header-welcome.component';
 import { HeaderMainComponent } from './shared/header-main/header-main.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { OperadorComponent } from './pages/operador/operador.component';
@@ -24,12 +24,12 @@ import { PacienteComponent } from './pages/paciente/paciente.component';
 import { NuevoTurnoComponent } from './pages/paciente/nuevo-turno/nuevo-turno.component';
 import { MisTurnosComponent } from './pages/paciente/mis-turnos/mis-turnos.component';
 import { MisDatosComponent } from './pages/paciente/mis-datos/mis-datos.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { TurnosProgramadosComponent } from './pages/medico/turnos-programados/turnos-programados.component';
 import { GestionAgendaComponent } from './pages/medico/gestion-agenda/gestion-agenda.component';
 import { MatTableModule } from '@angular/material/table';
@@ -40,9 +40,10 @@ import { TurnosDialogComponent } from './pages/operador/turnos-dialog/turnos-dia
 import { TurnoNotasDialogComponent } from './pages/medico/turnos-programados/turno-notas-dialog/turno-notas-dialog.component';
 import { TurnoConfirmadoDialogComponent } from './pages/paciente/nuevo-turno/turno-confirmado-dialog/turno-confirmado-dialog.component';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
-
-
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -88,11 +89,10 @@ import { TurnoConfirmadoDialogComponent } from './pages/paciente/nuevo-turno/tur
     MatIconModule,
     MatTableModule,
     MatSelectModule
-
-   
-
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
